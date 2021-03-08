3cae00e39d651bc80d13d109f3bfd9100b56fd40
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-03-09 04:34:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
+//Install express server    
 const express = require('express');
-const path = require('path');
 
-const app = express();
+const path = require('path');   
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+const app = express();   
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/frontend/'}),
-);
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
