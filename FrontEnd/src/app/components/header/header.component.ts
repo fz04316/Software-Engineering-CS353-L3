219a4c7a095f229b020273ae5d1ccfed2867b484
aiman-junaid <aj05161@st.habib.edu.pk>
2021-03-09 03:57:01 +0500

    ..

---
+++ FrontEnd/src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
+import {CartModelServer} from '../../models/cart.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  cartData: CartModelServer;
+  cartTotal: number;
+  constructor(public cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.cartTotal$.subscribe(total => {
+      this.cartTotal = total;
+    });
+
+    this.cartService.cartDataObs$.subscribe(data => this.cartData = data);
+  }
+
+}
