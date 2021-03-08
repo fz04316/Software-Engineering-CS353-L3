51c825708f636d3cabceff3b84d757b547ef3c58
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-03-09 01:54:39 +0500

    Add files via upload

---
+++ FrontEnd/src/app/components/cart/cart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
+import {CartModelServer} from '../../models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: number;
+  subTotal: number;
+
+  constructor(public cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.cartDataObs$.subscribe(data => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+  }
+  ChangeQuantity(id: number, increaseQuantity: boolean) {
+    this.cartService.UpdateCartData(id, increaseQuantity);
+  }
+}
