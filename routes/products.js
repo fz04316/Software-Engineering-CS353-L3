d62b3a72a148dd855e492cfaed64f358f090d9c3
aiman-junaid <aj05161@st.habib.edu.pk>
2021-04-05 01:34:01 +0500

    ..

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {database} = require('../config/helpers');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const database = mysql.createConnection({
     host: 'eu-cdbr-west-01.cleardb.com',
     user: 'b364ea95162c4a',
     password: '08a3b1c8',
     database: 'heroku_e9dd0a4d616598e'
 });
-connection.connect();
+database.connect();
 /* GET ALL PRODUCTS */
 router.get('/', function (req, res) {
   let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
@@ -73,7 +72,7 @@ router.post('/add', async (req, res) => {
 });
 
 router.put('/update', function (req, res) {
-        connection.query('UPDATE `products` SET `title`=?,`image`=?,`images`=?,`description`=?,`price`=?,`quantity`=?,`short_desc`=?,`cat_id`=? where `id`=?', [req.body.title,req.body.image, req.body.images,req.body.description,req.body.price,req.body.quantity,req.body.short_desc,req.body.cat_id, req.body.id], function (error, results, fields) {
+        database.query('UPDATE `products` SET `title`=?,`image`=?,`images`=?,`description`=?,`price`=?,`quantity`=?,`short_desc`=?,`cat_id`=? where `id`=?', [req.body.title,req.body.image, req.body.images,req.body.description,req.body.price,req.body.quantity,req.body.short_desc,req.body.cat_id, req.body.id], function (error, results, fields) {
             if (error) throw error;
             res.end(JSON.stringify(results));
         });
