76ffe8e42a5e0a57fa8e3f0267a8886291a5e47a
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-05 01:09:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var mysql=require('mysql');
 
 const app = express();
 
+var connection = mysql.createConnection({
+  host     : 'eu-cdbr-west-01.cleardb.com',
+  user     : 'b364ea95162c4a',
+  password : '08a3b1c8',
+  database : 'heroku_e9dd0a4d616598e'
+});
+connection.connect();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/backend'));
 
