05081ce695d037b3cc02ef236cabb897fa37ecf1
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-05 00:57:36 +0500

    Update helpers.js

--- config/helpers.js
+++ config/helpers.js
@@ -1,17 +1,9 @@
-const Mysqli = require('mysqli');
+var mysql = require('mysql');
+var connection = mysql.createConnection('mysql://b364ea95162c4a:08a3b1c8@eu-cdbr-west-01.cleardb.com/heroku_e9dd0a4d616598e?reconnect=true');
 
-
-let conn = new Mysqli({
-    Host: 'eu-cdbr-west-01.cleardb.com', // IP/domain name
-    user: 'b364ea95162c4a', // username
-    passwd: '08a3b1c8', // password
-    db: 'heroku_e9dd0a4d616598e'
-});
-
-let db = conn.emit(false, '');
+let db = connection.connect();
 
 module.exports = {
     database: db
 };
-
 
