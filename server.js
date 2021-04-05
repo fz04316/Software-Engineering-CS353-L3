61ff8b6c1b2e1b3d8650b7edcf898e09b40e9cba
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-05 05:45:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const Mysqli = require('mysqli');
-
-
-let conn = new Mysqli({
-    Host: 'eu-cdbr-west-01.cleardb.com', // IP/domain name
-    user: 'bbbf434d4e9277', // username
-    passwd: '578a4709', // password
-    db: 'heroku_ab3d640412d39ad'
-});
-
-conn.emit(false, '');
 
 
 
