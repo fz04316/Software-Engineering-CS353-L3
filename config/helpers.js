7588556940d7ddfa28ce8d148ebd9c251efec7be
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-05 05:46:45 +0500

    Update helpers.js

--- config/helpers.js
+++ config/helpers.js
@@ -2,16 +2,15 @@ const Mysqli = require('mysqli');
 
 
 let conn = new Mysqli({
-    Host: 'eu-cdbr-west-01.cleardb.com', // IP/domain name
-    user: 'bbbf434d4e9277', // username
-    passwd: '578a4709', // password
-    db: 'heroku_ab3d640412d39ad'
+    Host: 'localhost', // IP/domain name
+    port: 3306, // port, default 3306
+    user: 'root', // username
+    passwd: '', // password
+    db: 'mega_shop'
 });
 
-let db =  conn.emit(false, '');
+let db = conn.emit(false, '');
 
 module.exports = {
     database: db
 };
-
-
