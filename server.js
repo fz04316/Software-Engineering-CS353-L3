b370ce3a2e58b7a8059a0074314a412bb6c75b95
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-05 05:39:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Mysqli = require('mysqli');
+
+
+let conn = new Mysqli({
+    Host: 'eu-cdbr-west-01.cleardb.com', // IP/domain name
+    user: 'bbbf434d4e9277', // username
+    passwd: '578a4709', // password
+    db: 'heroku_ab3d640412d39ad'
+});
+
+conn.emit(false, '');
+
 
 
 // Serve only the static files form the dist directory
