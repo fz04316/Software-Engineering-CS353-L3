b3439b926a95a81b63b2b62f4f42933990e3d375
aiman-junaid <aj05161@st.habib.edu.pk>
2021-04-06 23:19:13 +0500

    ..

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const {database} = require('../config/helpers');
 const distDir = __dirname + "/dist/";
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(distDir));
