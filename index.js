de434fa64721c254e475e66fb996e4923a21d263
aiman-junaid <aj05161@st.habib.edu.pk>
2021-04-06 23:41:53 +0500

    ..

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express=require("express");
-const cors=require("cors");
-const {database} = require('../config/helpers');
+const express = require('express');
 const path = require('path');
-const app=express();
-var distDir = __dirname + "/dist/";
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(distDir));
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const index = express();
+const distDir = __dirname + "/dist/";
+index.use(express.static(__dirname + '/public'));
+index.use(express.static(distDir));
 const port = process.env.PORT || 3333;
-app.use(cors());
 
-//database endpoints will be here
-app.get('/',(request,response)=>{
-    response.json({info:'Node.js,Express, and Postgres API'});
+
+const productsRoute = require('./routes/products');
+
+/* CORS */
+index.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+
+
+index.use(logger('dev'));
+index.use(express.json());
+index.use(express.urlencoded({ extended: false }));
+index.use(cookieParser());
+index.use(express.static(path.join(__dirname, 'public')));
+
+index.use('/api/products',productsRoute);
+
+module.exports = index;
+index.get('/',(request, response)=>{
+  response.json({info:'Node.js,Express, and Postgres API'});
 });
-app.listen(port,()=>{
-    console.log(`App running on port ${port}.`);
+index.listen(port,()=>{
+  console.log(`App running on port ${port}.`);
 })
