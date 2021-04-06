c8c419e67449c7e0e770ee02708dd40729cf3739
aiman-junaid <aj05161@st.habib.edu.pk>
2021-04-07 00:51:24 +0500

    ..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/sprint1'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/sprint1/index.html'));
+app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
 app.listen(process.env.PORT || 8080);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   SERVER_URL : 'https://se-3dtraces.herokuapp.com/api/'
 };
 
