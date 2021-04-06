8e7e8d8bf292a54a6dfe3e5066d02cea5407d1e3
aiman-junaid <aj05161@st.habib.edu.pk>
2021-04-07 04:34:50 +0500

    ..

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:node app.js
+web:app node.js
--- app.js
+++ app.js
@@ -38,23 +38,8 @@ app.delete('/api/products/delete/:id',db.deleteproduct);
 
 app.listen(port,()=>{
   console.log(`App running on port ${port}.`);
-})
+});
 
 
 
 
-// define a sendmail endpoint, which will send emails and response with the corresponding status
-// app.post("/sendmail", (req, res) => {
-//     console.log("request came");
-//     let user = req.body;
-//     sendMail(user, (err, info) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(400);
-//         res.send({ error: "Failed to send email" });
-//       } else {
-//         console.log("Email has been sent");
-//         res.send(info);
-//       }
-//     });
-//   });
