d50279f939e062cddc38a5b6fd39311d5bd1c5c4
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-08 03:40:22 +0500

    Update product.component.ts

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -41,45 +41,6 @@ export class ProductComponent implements AfterViewInit, OnInit {
 
   }
 
-  ngAfterViewInit(): void {
-
-    // Product Main img Slick
-    $('#product-main-img').slick({
-      infinite: true,
-      speed: 300,
-      dots: false,
-      arrows: true,
-      fade: true,
-      asNavFor: '#product-imgs',
-    });
-
-    // Product imgs Slick
-    $('#product-imgs').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      arrows: true,
-      centerMode: true,
-      focusOnSelect: true,
-      centerPadding: 0,
-      vertical: true,
-      asNavFor: '#product-main-img',
-      responsive: [{
-        breakpoint: 991,
-        settings: {
-          vertical: false,
-          arrows: false,
-          dots: true,
-        }
-      },
-      ]
-    });
-
-    // Product img zoom
-    const zoomMainProduct = document.getElementById('product-main-img');
-    if (zoomMainProduct) {
-      $('#product-main-img .product-preview').zoom();
-    }
-  }
 
   addToCart(id: number) {
     this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
