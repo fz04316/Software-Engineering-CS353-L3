f7eecfd7c95ced9e2597221cc02d6e31b5de8ba7
aiman-junaid <54744119+aiman-junaid@users.noreply.github.com>
2021-04-08 03:56:35 +0500

    Update product.component.ts

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -12,7 +12,7 @@ declare let $: any;
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements AfterViewInit, OnInit {
   id: number;
   product;
   thumbimages: any[] = [];
@@ -41,6 +41,45 @@ export class ProductComponent implements OnInit {
 
   }
 
+  ngAfterViewInit(): void {
+
+    // Product Main img Slick
+    $('#product-main-img').slick({
+      infinite: true,
+      speed: 300,
+      dots: false,
+      arrows: true,
+      fade: true,
+      asNavFor: '#product-imgs',
+    });
+
+    // Product imgs Slick
+    $('#product-imgs').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      centerMode: true,
+      focusOnSelect: true,
+      centerPadding: 0,
+      vertical: true,
+      asNavFor: '#product-main-img',
+      responsive: [{
+        breakpoint: 991,
+        settings: {
+          vertical: false,
+          arrows: false,
+          dots: false,
+        }
+      },
+      ]
+    });
+
+    // Product img zoom
+    const zoomMainProduct = document.getElementById('product-main-img');
+    if (zoomMainProduct) {
+      $('#product-main-img .product-preview').zoom();
+    }
+  }
 
   addToCart(id: number) {
     this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
